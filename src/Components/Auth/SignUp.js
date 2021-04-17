e220794d4c1cb51d902cd1d1974b18cfccf2e67d
harsh-heda <harsh.heda20@gmail.com>
2021-04-17 21:21:19 +0530

    login form validation added

--- src/Components/Auth/SignUp.js
+++ src/Components/Auth/SignUp.js
@@ -3,33 +3,37 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { MenuItem, Menu, List, ListItem, ListItemText } from '@material-ui/core';
+import { MenuItem} from '@material-ui/core';
 import serverUrl from "../../serverURL";
-import { render } from 'react-dom';
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import {
   minMaxLength,
   validEmail,
   passwordStrength,
-  userExists,
 } from './validation';
 
 var FormData = require('form-data');
 
 const roles = [
-  'Customer',
-  'Retailer',
-  'Wholesaler',
+  {
+    value: 'customer',
+    label: 'Customer',
+  },
+  {
+    value: 'retailer',
+    label: 'Retailer',
+  },
+  {
+    value: 'wholesaler',
+    label: 'Wholesaler',
+  },
 ];
 
 const useStyles = makeStyles((theme) => ({
@@ -61,8 +65,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp() {
   const classes = useStyles();
-  const [jwt, setJWT] = useState(null);
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   const [role, setRole] = useState(0);
   const [username, setUsername] = useState('');
@@ -150,25 +152,10 @@ export default function SignUp() {
     setPassword2(e.target.value);
     setError({ ...error, confirmPassword: '' })
   }
-
-  const handleClickListItem = (event) => {
-    setAnchorEl(event.currentTarget);
-  }
-
   const handleMenuItemClick = (event, index) => {
     setSelectedIndex(index);
-    setAnchorEl(null);
     setRole(index);
   }
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  }
-  // React.useEffect(() => {
-  //   return () => (
-  //     cleanup
-  //   )
-  // }, [])
   const handleUserNameError = () => {
     if (minMaxLength(username, 3)) {
       setError({ ...error, userName: 'Must be greater than 3 character' });
@@ -177,7 +164,7 @@ export default function SignUp() {
   const handleMobError = () => {
     console.log(phone.length);
     let reg = new RegExp(/^\d*$/).test(phone);
-    if (phone.length != 10)
+    if (phone.length !== 10)
       setError({ ...error, mob: 'Must be 10 digit' });
     else if (!reg)
       setError({ ...error, mob: 'Only number permitted' });
@@ -197,7 +184,7 @@ export default function SignUp() {
       setError({ ...error, password: 'Must be greater than 6 character' });
     else if (passwordStrength(password))
       setError({ ...error, password: 'Password is not strong enough. Include an upper case letter, a number or a special character to make it strong' });
-    if (password2 != '') {
+    if (password2 !== '') {
       validateConfirmPassword(
         password,
         password2,
@@ -205,7 +192,7 @@ export default function SignUp() {
     }
   }
   const handleConfirmPasswordError = () => {
-    let valid = validateConfirmPassword(
+     validateConfirmPassword(
       password,
       password2,
     );
@@ -221,7 +208,7 @@ export default function SignUp() {
       });
       return false;
     }
-    else if (error['password'] != '') {
+    else if (error['password'] !== '') {
       setError({
         ...error, confirmPassword:
           error['password']
@@ -247,27 +234,7 @@ export default function SignUp() {
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} >
-              <TextField
-                autoFocus
-                autoComplete="userName"
-                name="userName"
-                variant="outlined"
-                required
-                fullWidth
-                id="userName"
-                label="Username"
-                autoFocus
-                onBlur={handleUserNameError}
-                error={error['userName'] !== ''}
-                helperText={error['userName']}
-                InputLabelProps={{
-                  className: classes.floatingLabelFocusStyle
-                }}
-                onChange={handleUsername}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} >
+            <Grid item xs={12} >
               <TextField
                 autoComplete="name"
                 name="name"
@@ -285,37 +252,46 @@ export default function SignUp() {
                 onChange={handleName}
               />
             </Grid>
+            <Grid item xs={12}  sm={6}>
+              <TextField
+                autoFocus
+                autoComplete="userName"
+                name="userName"
+                variant="outlined"
+                required
+                fullWidth
+                id="userName"
+                label="Username"
+                onBlur={handleUserNameError}
+                error={error['userName'] !== ''}
+                helperText={error['userName']}
+                InputLabelProps={{
+                  className: classes.floatingLabelFocusStyle
+                }}
+                onChange={handleUsername}
+              />
+            </Grid>
             <Grid item xs={12} sm={6}>
-
-              <List component="nav" aria-label="User Type">
-                <ListItem
-                  button
-                  aria-haspopup="true"
-                  aria-controls="user-type"
-                  aria-label="User Type"
-                  onClick={handleClickListItem}
-                >
-                  <ListItemText primary="User Role" secondary={roles[selectedIndex]} />
-                </ListItem>
-              </List>
-
-              <Menu
-                id="user-type"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                {roles.map((option, index) => (
-                  <MenuItem key={option.value}
-                    value={option.value}
+            <TextField
+                    id='role'
+                    select
+                    fullWidth
+                    label="Role"
+                    //value={role}
+                    //onChange={handleChange}
+                    InputLabelProps={{
+                        className: classes.floatingLabelFocusStyle
+                    }}
+                    variant="outlined"
+                    >
+                    {roles.map((option,index) => (
+                        <MenuItem key={option.value}value={option.value}
                     selected={index === selectedIndex}
-                    onClick={(event) => handleMenuItemClick(event, index)}
-                  >
-                    {option}
-                  </MenuItem>
-                ))}
-              </Menu>
+                    onClick={(event) => handleMenuItemClick(event, index)}>
+                        {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>              
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -397,7 +373,12 @@ export default function SignUp() {
             color="primary"
             className={classes.submit}
             onClick={handleSubmit}
-          // disabled={error['name']!==''}
+           disabled={error['userName']!=='' ||
+            error['name']!==''||
+            error['mail']!==''||
+            error['password']!==''|| 
+            error['confirmPassword']!==''||
+            error['mob']!==''}
           >
             Sign Up
           </Button>
