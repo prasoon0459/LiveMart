ab662c1299087ea75b9fdd9548d2824689c77800
Shaurya <shaurya.vijayvargiya@gmail.com>
2021-04-27 11:11:31 +0530

    Changes Server Url to Heroku Backend

--- src/serverURL.js
+++ src/serverURL.js
@@ -1,3 +1,3 @@
-const serverUrl = "http://127.0.0.1:8000";
-// const serverUrl = "https://scamazon-oops.herokuapp.com";
+// const serverUrl = "http://192.168.29.159:8000";
+const serverUrl = "https://scamazon-oops.herokuapp.com";
 export default serverUrl;
